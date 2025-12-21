469a652be74c0638d492f7565f709a2b6169613a
George M <georgem17636315081@outlook.com>
2025-12-21 08:29:40 +0000

    Fix typo

--- reasoning_features/bash/run_steering_experiment.sh
+++ reasoning_features/bash/run_steering_experiment.sh
@@ -4,13 +4,13 @@ for layer in 0 4 8 12 16 20 24; do
         --features-file results/initial-setting/gemma-2-2b/s1k/layer$layer/reasoning_features.json \
         --top-k-features 10 \
         --benchmark aime24 \
-        --save-dir results/initial-setting/gemma-2-2b/s1k/layer$layer/aime24 \
+        --save-dir results/initial-setting/gemma-2-2b/s1k/layer$layer/aime24
     python reasoning_features/scripts/run_steering_experiment.py \
         --layer $layer \
         --features-file results/initial-setting/gemma-2-2b/s1k/layer$layer/reasoning_features.json \
         --top-k-features 10 \
         --benchmark gpqa_diamond \
-        --save-dir results/initial-setting/gemma-2-2b/s1k/layer$layer/gpqa_diamond \
+        --save-dir results/initial-setting/gemma-2-2b/s1k/layer$layer/gpqa_diamond
 done
 
 for layer in 0 4 8 12 16 20 24; do
@@ -19,11 +19,11 @@ for layer in 0 4 8 12 16 20 24; do
         --features-file results/initial-setting/gemma-2-2b/general_inquiry_cot/layer$layer/reasoning_features.json \
         --top-k-features 10 \
         --benchmark aime24 \
-        --save-dir results/initial-setting/gemma-2-2b/general_inquiry_cot/layer$layer/aime24 \
+        --save-dir results/initial-setting/gemma-2-2b/general_inquiry_cot/layer$layer/aime24
     python reasoning_features/scripts/run_steering_experiment.py \
         --layer $layer \
         --features-file results/initial-setting/gemma-2-2b/general_inquiry_cot/layer$layer/reasoning_features.json \
         --top-k-features 10 \
         --benchmark gpqa_diamond \
-        --save-dir results/initial-setting/gemma-2-2b/general_inquiry_cot/layer$layer/gpqa_diamond \
+        --save-dir results/initial-setting/gemma-2-2b/general_inquiry_cot/layer$layer/gpqa_diamond
 done
